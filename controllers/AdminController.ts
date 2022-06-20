867c2859c8c542ab253bd4a940cff79514da2090
Swift420 <apollosdavid95@gmail.com>
2022-06-20 13:52:12 +0200

    Vendor Model

--- controllers/AdminController.ts
+++ controllers/AdminController.ts
@@ -1,10 +1,25 @@
 import { Request, Response,NextFunction} from "express"
 import { CreateVendorInput } from "../dto";
-
+import { Vendor } from "../models";
 
 export const CreateVendor = async (req: Request, res: Response, next: NextFunction) => {
 
     const { name, address,pincode,foodType,email,password,ownerName,phone} = <CreateVendorInput>req.body;
+
+    const createdVendor = await Vendor.create({
+        name: name,
+        address: address,
+        pincode: pincode,
+        foodType: foodType,
+        email: email,
+        password: password,
+        salt: '',
+        ownerName: ownerName,
+        phone: phone,
+        rating: 0,
+        serviceAvailable: false,
+        coverImages: [],
+    })
 
     return res.json({name, address,pincode,email,password,ownerName,phone, foodType})
 
