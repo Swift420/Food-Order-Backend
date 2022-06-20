2528b2b2b11fc168b8aab795f042b37165a6b8cc
Swift420 <apollosdavid95@gmail.com>
2022-06-20 13:30:16 +0200

    vendor dto

--- controllers/AdminController.ts
+++ controllers/AdminController.ts
@@ -1,8 +1,12 @@
 import { Request, Response,NextFunction} from "express"
+import { CreateVendorInput } from "../dto";
 
 
 export const CreateVendor = async (req: Request, res: Response, next: NextFunction) => {
 
+    const { name, address,pincode,foodType,email,password,ownerName,phone} = <CreateVendorInput>req.body;
+
+    return res.json({name, address,pincode,email,password,ownerName,phone, foodType})
 
 }
 
