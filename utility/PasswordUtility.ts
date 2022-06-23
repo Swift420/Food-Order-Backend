3e951388fa7d7043fc46ad58c54c64a429bdf215
Swift420 <apollosdavid95@gmail.com>
2022-06-23 15:31:54 +0200

    common auth middleware

--- utility/PasswordUtility.ts
+++ utility/PasswordUtility.ts
@@ -2,7 +2,8 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import { APP_SECRET } from "../config";
 import { VendorPayload } from "../dto";
-
+import { Request } from "express"
+import { AuthPayload } from "../dto/Auth.dto";
 
 export const GenerateSalt = async () => {
     return await bcrypt.genSalt(10);
@@ -24,4 +25,20 @@ export const GenerateSignature =  (payload: VendorPayload) => {
   return jwt.sign(payload, APP_SECRET, {expiresIn: "1d"})
 
    
+}
+
+export const validateSignature = async(req:Request) => {
+
+  const signature = req.get("Authorization")
+
+  if(signature) {
+
+    const payload = await jwt.verify(signature.split(" ")[1], APP_SECRET) as AuthPayload
+
+    req.user = payload;
+
+    return true
+
+  }
+  return false
 }
